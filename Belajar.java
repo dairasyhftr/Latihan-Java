3c7822747456349e030a51078be60a6a8914132a
dairasyhftr <dairasyahfitri14.gmail.com>
2024-03-24 15:54:18 +0700

    latihan1

---
+++ Belajar.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.latihan.java;
+
+/**
+ *
+ * @author DAIRA SYAHFITRI
+ */
+class Belajar 
+{
+public String x = "Pintar";
+private String y = "Java";
+}
